8dfcd98c1622789ade8abfd333f200ab0122928c
lovelyzzc <1439929642@qq.com>
2020-03-11 18:33:40 +0800

    Courser 2 Week 1

---
+++ Improving Deep Neural Networks Hyperparameter/Week1/initialization/Initiation.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
+train_X, trainY, test_X, test_Y = load_dataset()
+
+
+def model(X, Y, learning_rate=0.01, num_iterations = 15000, print_cost=True, initialization='he'):
+    grads = {}
+    costs = []
+    m = X.shape[1]
+    layer_dims = [X.shape[0], 10, 5, 1]
+    if initialization == 'zeros':
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == 'random':
+        parameters = initialize_parameters_random(layer_dims)
+    elif initialization == 'he':
+        parameters = initialize_parameters_he(layer_dims)
+
+    for i in range(0, num_iterations):
+        a3, cache = forward_propagation(X, parameters)
+
+        cost = compute_loss(a3, Y)
+
+        grads = backward_propagation(X, Y, cache)
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        if print_cost and i % 100 == 0:
+            print('Cost after iteration{}: {}'.format(i, cost))
+            costs.append(cost)
+
+    plt.plot(costs)
+    plt.ylabel('Cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title('Learning rate = ' + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
+def initialize_parameters_zeros(layers_dims):
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l-1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+
+def initialize_parameters_random(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+
+def initialize_parameters_he(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * np.sqrt(2/layers_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
